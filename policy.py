d6e0701c3e4418602cdfb33546bfa7d80039f2f5
Shravan Kumaran <shravankumaran@BlackSpiral.local>
2019-12-12 15:02:17 -0800

    fixed state update when running episodes

--- policy.py
+++ policy.py
@@ -1,9 +1,13 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Dense, Flatten, Conv2D
 from tensorflow.keras.optimizers import Adam
 from envs.gridworld_env import GridworldEnv
+
+tf.keras.backend.set_floatx('float64')
 
 ### Base Classes ###
 
@@ -74,70 +78,67 @@ class BaseDistralTrainer:
 	def get_policies(self, policy_num):
 		raise NotImplementedError
 
-	def train(self, num_episodes=1000, max_num_steps_per_episode=100, save=False):
-		if save:
-			episode_rewards = [[] for i in range(num_episodes)]
-			episode_durations = [[] for i in range(num_episodes)]
+	def run_episode(self, policy_num, env, max_num_steps_per_episode):
+		total_reward = 0
+		duration = 0
+
+		state = env.reset()
+		distilled, distilled_opt = self.get_distilled(policy_num)
+		policy, policy_opt = self.get_policies(policy_num)
+
+		with tf.GradientTape() as policy_tape, tf.GradientTape() as distilled_tape:
+			discount = 1
+			reward_loss = 0
+			distilled_loss = 0
+			entropy_loss = 0 
+
+			for t in range(max_num_steps_per_episode):
+				policy_probs = policy(state)
+				distilled_probs = distilled(state)
+
+				action = tf.math.argmax(policy_probs, axis=1) # Change to sample from distribution
+				action = int(action) #Cast to integer
+
+				policy_log_prob = tf.math.log(policy_probs[0][action])
+				distilled_log_prob = tf.math.log(distilled_probs[0][action])
+
+				next_state, reward, done, _ = env.step(action)
+
+				reward_loss += -discount * reward
+				distilled_loss += -discount * self.alpha / self.beta * distilled_log_prob
+				entropy_loss += discount / self.beta * policy_log_prob
+
+				state = next_state
+				discount *= self.gamma
+				total_reward += reward
+				duration += 1
+
+				if done:
+					break
+
+			loss = reward_loss + distilled_loss + entropy_loss
+
+		policy_gradients = policy_tape.gradient(loss, policy.trainable_variables)
+		policy_opt.apply_gradients(zip(policy_gradients, policy.trainable_variables))
+
+		distilled_gradients = distilled_tape.gradient(loss, distilled.trainable_variables)
+		distilled_opt.apply_gradients(zip(distilled_gradients, distilled.trainable_variables))
+
+		return loss, total_reward, duration
+
+	def train(self, num_episodes=1000, max_num_steps_per_episode=100):
+		episode_rewards = [[] for i in range(num_episodes)]
+		episode_durations = [[] for i in range(num_episodes)]
 
 		for ep_num in range(num_episodes):
 			for i, env in enumerate(self.envs):
-				if save:	
-					total_reward = 0
-					duration = 0
+				loss, total_reward, duration = self.run_episode(i, env, max_num_steps_per_episode)
 
-				state = env.reset()
-				distilled, distilled_opt = self.get_distilled(i)
-				policy, policy_opt = self.get_policies(i)
+				print(i, loss, total_reward)
+				episode_rewards[ep_num].append(total_reward)
+				episode_durations[ep_num].append(duration)
 
-				with tf.GradientTape() as policy_tape, tf.GradientTape() as distilled_tape:
-					discount = 1
-					reward_loss = 0
-					distilled_loss = 0
-					entropy_loss = 0 
-
-					for t in range(max_num_steps_per_episode):
-						policy_probs = policy(state)
-						distilled_probs = distilled(state)
-
-						action = tf.math.argmax(policy_probs, axis=1) # Change to sample from distribution
-
-						#Cast to integer
-						action = int(action)
-
-						policy_log_prob = tf.math.log(policy_probs[0][action])
-						distilled_log_prob = tf.math.log(distilled_probs[0][action])
-
-
-						next_state, reward, done, _ = env.step(action)
-
-						reward_loss += -discount * reward
-						distilled_loss += -discount * self.alpha / self.beta * distilled_log_prob
-						entropy_loss += discount / self.beta * policy_log_prob
-
-						discount *= self.gamma
-						if save:
-							total_reward += reward
-							duration += 1
-
-						if done:
-							break
-
-					loss = reward_loss + distilled_loss + entropy_loss
-					if ep_num % 10 == 0:
-						print(i, loss, total_reward)
-
-				policy_gradients = policy_tape.gradient(loss, policy.trainable_variables)
-				policy_opt.apply_gradients(zip(policy_gradients, policy.trainable_variables))
-
-				distilled_gradients = distilled_tape.gradient(loss, distilled.trainable_variables)
-				distilled_opt.apply_gradients(zip(distilled_gradients, distilled.trainable_variables))
-
-				if save:
-					episode_rewards[ep_num].append(total_reward)
-					episode_durations[ep_num].append(duration)
-
-		if save:
-			return episode_rewards, episode_durations
+		return episode_rewards, episode_durations
 
 ## Specific Classes ##
 
@@ -160,23 +161,26 @@ class RegularDistralTrainer(BaseDistralTrainer):
 
 class HeirarchicalDistralTrainer(BaseDistralTrainer):
 	def __init__(self, envs, alpha=0.5, beta=0.005, gamma=0.8,
-		learning_rate=0.001, layer_size=64, depth=2):
+		learning_rate=0.001, layer_size=64, depth=2,
+		num_distilled=2, c=0.005):
 		super(HeirarchicalDistralTrainer, self).__init__(envs, alpha, beta, gamma, learning_rate, layer_size, depth)
+		self.num_distilled = num_distilled
+		self.c = 0.005
 
 	def make_distilled(self):
-		raise NotImplementedError
+		return [self.make_policy() for _ in range(self.num_distilled)], [Adam(self.learning_rate) for _ in range(self.num_distilled)]
 
 	def make_policies(self):
-		raise NotImplementedError
+		return [self.make_policy() for _ in range(self.num_tasks)], [Adam(self.learning_rate) for _ in range(self.num_tasks)]
 
 	def get_distilled(self, policy_num):
 		raise NotImplementedError
 
 	def get_policies(self, policy_num):
-		raise NotImplementedError
+		return self.policies[policy_num], self.policies_opt[policy_num]
 
 ### Run Model ###
 def train_distral(envs=[GridworldEnv(5), GridworldEnv(4)]):
 	distral_trainer = RegularDistralTrainer(envs)
-	episode_rewards, episode_durations = distral_trainer.train(save=True)
-	print(episode_rewards, episode_durations)
+	episode_rewards, episode_durations = distral_trainer.train()
+	return episode_rewards, episode_durations
