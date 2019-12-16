c5546f1a78ef3a69888961dd1d127af6d68f665d
PeterDykas <peter.dykas@berkeley.edu>
2019-12-15 23:25:43 -0800

    Push the pytorch

---
+++ Distral_pytorch/network.py
@@ -0,0 +1,127 @@
+import math
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+from memory_replay import Transition
+from itertools import count
+from torch.distributions import Categorical
+
+class DQN(nn.Module):
+    """
+    Deep neural network with represents an agent.
+    """
+    def __init__(self, input_size, num_actions):
+        super(DQN, self).__init__()
+        self.linear1 = nn.Linear(input_size, 50)
+        self.linear2 = nn.Linear(50, 50)
+        self.head = nn.Linear(50, num_actions)
+
+    def forward(self, x):
+        x = F.leaky_relu(self.linear1(x))
+        x = F.leaky_relu(self.linear2(x))
+        return self.head(x)
+
+class PolicyNetwork(nn.Module):
+    """
+    Deep neural network which represents policy network.
+    """
+    def __init__(self, input_size, num_actions):
+        super(PolicyNetwork, self).__init__()
+        self.linear1 = nn.Linear(input_size, 50)
+        self.linear2 = nn.Linear(50, 50)
+        self.head = nn.Linear(50, num_actions)
+
+    def forward(self, x):
+        x = F.leaky_relu(self.linear1(x))
+        x = F.leaky_relu(self.linear2(x))
+        return F.softmax(self.head(x))
+
+def select_action(state, policy, model, alpha, beta):
+    """
+    Selects whether the next action is choosen by our model or randomly
+    """
+
+    #TODO: Make sure that I am formatting the state correctly
+    Q = model(state)
+    pi0 = policy(state)
+    # print(pi0.data.numpy())
+    V = torch.log((torch.pow(pi0, alpha) * torch.exp(beta * Q)).sum(1) ) / beta
+
+    pi_i = torch.pow(pi0, alpha) * torch.exp(beta * (Q - V))
+    m = Categorical(pi_i)
+
+    #TODO: Probably need to fix this
+    action = m.sample()
+    return action
+
+def optimize_policy(policy, optimizer, memories, batch_size,
+                    num_envs, gamma):
+    loss = 0
+    for i_env in range(num_envs):
+        size_to_sample = np.minimum(batch_size, len(memories[i_env]))
+        transitions = memories[i_env].policy_sample(size_to_sample)
+        batch = Transition(*zip(*transitions))
+        
+        state_batch = torch.tensor(torch.cat(batch.state))
+        time_batch = torch.tensor(torch.cat(batch.time))
+        actions = np.array([action.numpy() for action in batch.action])
+
+        cur_loss = (torch.pow(torch.tensor([gamma]), time_batch) * torch.log(policy(state_batch)[:, actions])).sum()
+        loss -= cur_loss
+
+    optimizer.zero_grad()
+    loss.backward()
+
+    # for param in policy.parameters():
+    #     param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+
+def optimize_model(policy, model, optimizer, memory, batch_size, alpha, beta, gamma):
+    if len(memory) < batch_size:
+        return
+    transitions = memory.sample(batch_size)
+    # Transpose the batch (see http://stackoverflow.com/a/19343/3343043 for
+    # detailed explanation).
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)))
+
+
+    # We don't want to backprop through the expected action values and volatile
+    # will save us on temporarily changing the model parameters'
+    # requires_grad to False!
+    non_final_next_states = torch.tensor(torch.cat([s for s in batch.next_state if s is not None]))
+
+    #TODO: This is going to need a finer look(especially action_batch)
+    state_batch = torch.tensor(torch.cat(batch.state))
+    action_batch = torch.tensor(torch.stack(batch.action)).view(-1,1)
+    reward_batch = torch.tensor(torch.cat(batch.reward))
+
+    #TODO: Make sure that this is working correctly
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the columns of actions taken
+    state_action_values = model(state_batch)
+    state_action_values = state_action_values.gather(1, action_batch)
+
+
+    # Compute V(s_{t+1}) for all next states.
+    next_state_values = torch.tensor(torch.zeros(batch_size))
+    next_state_values[non_final_mask] = torch.log((torch.pow(policy(non_final_next_states), alpha) * (torch.exp(beta * model(non_final_next_states)) + 1e-16)).sum(1)) / beta
+
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * gamma) + reward_batch
+
+    # Compute Huber loss
+    loss = F.mse_loss(state_action_values, expected_state_action_values)
+
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    # for param in model.parameters():
+    #     param.grad.data.clamp_(-1, 1)
+    optimizer.step()
---
+++ Distral_pytorch/trainingDistral2col0.py
@@ -0,0 +1,135 @@
+import torch.optim as optim
+import torch
+import math
+import numpy as np
+from memory_replay import ReplayMemory, Transition
+from network import DQN, select_action, optimize_model, optimize_policy, PolicyNetwork
+import sys
+sys.path.append('../')
+from envs.gridworld_env import GridworldEnv
+import gym
+import gym_minigrid
+
+
+def trainD(file_name="Distral_1col", list_of_envs=[GridworldEnv(5),
+            GridworldEnv(4), GridworldEnv(6)], batch_size=128, gamma=0.999, alpha=0.5,
+            beta=.5, eps_start=0.9, eps_end=0.05, eps_decay=5, num_episodes=200,
+            max_num_steps_per_episode=500, learning_rate=0.001,
+            memory_replay_size=10000, memory_policy_size=1000):
+    """
+    Soft Q-learning training routine. Retuns rewards and durations logs.
+    Plot environment screen
+    """
+    num_actions = list_of_envs[0].action_space.n
+    try:
+        input_size = list_of_envs[0].observation_space.shape[0]
+    except:
+        input_size = np.prod(list_of_envs[0].observation_space['image'].shape)
+
+    num_envs = len(list_of_envs)
+    policy = PolicyNetwork(input_size, num_actions)
+    models = [DQN(input_size,num_actions) for _ in range(0, num_envs)]   ### Add torch.nn.ModuleList (?)
+    memories = [ReplayMemory(memory_replay_size, memory_policy_size) for _ in range(0, num_envs)]
+
+    use_cuda = torch.cuda.is_available()
+    if use_cuda:
+        print("Using CUDA")
+        policy.cuda()
+        for model in models:
+            model.cuda()
+
+    optimizers = [optim.Adam(model.parameters(), lr=learning_rate) for model in models]
+    policy_optimizer = optim.Adam(policy.parameters(), lr=learning_rate)
+
+    episode_durations = [[] for _ in range(num_envs)]
+    episode_rewards = [[] for _ in range(num_envs)]
+
+    steps_done = np.zeros(num_envs)
+    episodes_done = np.zeros(num_envs)
+    current_time = np.zeros(num_envs)
+
+    # Initialize environments
+    states = []
+    for env in list_of_envs:
+        #Format = tensor([[-0.7778, -0.5000,  0.0000]])
+        state =  env.reset()
+
+        #If from the gym mini grid environment
+        if isinstance(state,dict):
+            state = state['image']
+
+        state = torch.from_numpy(state).type(torch.FloatTensor).view(-1,input_size)
+        states.append(state)
+
+    while np.min(episodes_done) < num_episodes:
+
+        # Optimization is given by alterating minimization scheme:
+        #   1. do the step for each env
+        #   2. do one optimization step for each env using "soft-q-learning".
+        #   3. do one optimization step for the policy
+
+        for i_env, env in enumerate(list_of_envs):
+
+            action = select_action(states[i_env], policy, models[i_env], alpha, beta)
+            action = action.data[0]
+
+            steps_done[i_env] += 1
+            current_time[i_env] += 1
+            next_state_tmp, reward, done, _ = env.step(action)
+            reward = torch.tensor([reward])
+
+            # Observe new state
+            # If from the gym mini grid environment
+            if isinstance(next_state_tmp, dict):
+                next_state_tmp = next_state_tmp['image']
+
+            next_state = torch.from_numpy( next_state_tmp ).type(torch.FloatTensor).view(-1,input_size)
+
+
+            if done:
+                next_state = None
+
+            # Store the transition in memory
+            time = torch.tensor([current_time[i_env]])
+            memories[i_env].push(states[i_env], action, next_state, reward, time)
+
+            # Perform one step of the optimization (on the target network)
+            optimize_model(policy, models[i_env], optimizers[i_env], memories[i_env], batch_size, alpha, beta, gamma)
+
+            # Update state
+            states[i_env] = next_state
+
+            #TODO: make sure this is the correct way to handle max_num_steps_per_episode
+            # Check if agent reached target
+            if done or current_time[i_env] >= max_num_steps_per_episode:
+                print("ENV:", i_env, "iter:", episodes_done[i_env],
+                    "\treward:", env.episode_total_reward,
+                    "\tit:", current_time[i_env], "\texp_factor:", eps_end +
+                    (eps_start - eps_end) * math.exp(-1. * episodes_done[i_env] / eps_decay))
+                states[i_env] = torch.from_numpy( env.reset() ).type(torch.FloatTensor).view(-1,input_size)
+                episodes_done[i_env] += 1
+                episode_durations[i_env].append(current_time[i_env])
+                current_time[i_env] = 0
+                episode_rewards[i_env].append(env.episode_total_reward)
+
+        optimize_policy(policy, policy_optimizer, memories, batch_size,num_envs, gamma)
+
+    np.save(file_name + '-distral-2col-rewards', episode_rewards)
+    np.save(file_name + '-distral-2col-durations', episode_durations)
+
+    return models, policy, episode_rewards, episode_durations
+
+def empty_room(size = 6, wrapper = None,n = 4):
+    if size != 5 and size != 6:
+        print("Invalid Size")
+        return
+    env_type = "MiniGrid-Empty-Random-" + str(size)+ "x" + str(size) +"-v0"
+    if wrapper is None:
+        envs = [gym.make(env_type) for _ in range(n)]
+    else:
+        envs = [wrapper(gym.make(env_type)) for _ in range(n)]
+    return envs
+
+
+
+trainD(list_of_envs=empty_room(n=3))
